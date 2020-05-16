6c46c1e6804669e2590b5e84e885a0c1409d10ca
Vanderson Alves <vanalvesdev@gmail.com>
2020-05-16 17:23:44 -0300

    first commit

---
+++ src/main/java/com/vaanalvesr/generatechar/GeneratecharApplication.java
@@ -0,0 +1,13 @@
+package com.vaanalvesr.generatechar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratecharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratecharApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaanalvesr/generatechar/GeneratecharApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaanalvesr.generatechar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratecharApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
